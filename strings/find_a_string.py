20e4d58b48ae504bb985173274d7d61d6155be13
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-05-20 23:58:47 -0400

    added a simple function caller for the string finding challenge

--- strings/find_a_string.py
+++ strings/find_a_string.py
@@ -5,3 +5,9 @@ def count_substring(string, sub_string):
 				if string[x:x + len(sub_string)] == sub_string:
 						count += 1
 		return count
+
+if __name__ == '__main__':
+		string = raw_input().strip()
+		sub_string = raw_input.strip()
+		count = count_substring(string, sub_string)
+		print(count)
